b6813d2b6a43c7a6ec559d8c038ea4a50285f9b3
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2024-09-16 17:31:51 +0600

    ok

---
+++ components/From.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const From = () => {
+    
+  return (
+    <>
+    <div className='text-center'>
+        <h1 className='text-center '>This is a From</h1>
+        <input type="text" name="UserName" id=""  className='bg-red-200'/>
+        <button type="submit" className='bg-lime-300'>Submit</button>
+    </div>
+    </>
+  )
+}
+
+export default From
